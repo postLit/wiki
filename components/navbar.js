82c8a69b2ee1b14f5d3bc5e4deaab9c9c91fec68
rgantzos <info@gantzos.com>
2022-12-24 14:54:22 -0800

    postlit

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+exports.navbar = `
+<div class="navbar">
+      <a href="/"><span><b>postLit</b> wiki</span></a>
+      <div class="right">
+      <a href="/random/"><span>Random Page</span></a>
+      </a>
+    </div>
+    </div>
+`;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { navbar } = require("./components/navbar.js")
+
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
@@ -29,6 +31,7 @@ Object.prototype.sendRenderedFile = async function(file, info) {
   var res = this
   fs.readFile(path.join(__dirname, file), function(err, data) {
     var text = data.toString()
+    text = text.replace("<body>", "<body>"+navbar)
     Object.keys(info).forEach(function(el) {
     text = text.replaceAll("<%= "+el+" %>", info[el])
   })
@@ -36,14 +39,49 @@ Object.prototype.sendRenderedFile = async function(file, info) {
   })
 }
 
-app.get("/", function(req, res) {
-    res.sendRenderedFile("/pages/main.html", {
-      text: "<img src='/images/main.png'><h4><a href='/wiki/postlit/'>postLit.dev</a></h34<a href='/users/rgantzos/'>yeah</a> i agree with that entirely".toString()
+function getPageData(file, part) {
+  try {
+  var text = fs.readFileSync(path.join(__dirname, file), 'utf8')
+  if (part) {
+    const parts = {
+      title: ['<h1 class="title">', '</h1>']
+    }
+    text = text.split(parts[part][0])[1].split(parts[part][1])[0]
+  }
+  return text.toString()
+  } catch(err) {
+    return null
+  }
+}
+
+app.get("/", async function(req, res) {
+    res.sendRenderedFile("/html/main.html", {
+      text: getPageData("/pages/postlit-dev.html"),
+      box2Head: "Verification",
+      box2Text: getPageData("/pages/verification.html"),
+      box3Head: "Moderation",
+      box3Text: getPageData("/pages/moderation.html")
     })
 })
 
+app.get("/random/", async function(req, res) {
+  var files = fs.readdirSync(path.join(__dirname, "/pages"))
+  res.redirect("/pages/"+(files[Math.floor(Math.random() * files.length)]).replace(".html", "")+"/")
+})
+
+app.get("/pages/:page/", async function(req, res, next) {
+  if (getPageData("/pages/"+req.params.page+".html")) {
+    res.sendRenderedFile("/html/page.html", {
+      title: getPageData("/pages/"+req.params.page+".html", "title"),
+      content: getPageData("/pages/"+req.params.page+".html")
+    })
+  } else {
+    next()
+  }
+})
+
 app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "/pages/404.html"))
+  res.sendFile(path.join(__dirname, "/html/404.html"))
 })
 
 app.listen(PORT);
